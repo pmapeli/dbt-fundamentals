676f82d4c97b088d6954ac69ce07ae174628b1ef
paulo.mapeli.ext@siemens.com <paulo.mapeli.ext@siemens.com>
2022-02-18 16:00:32 +0000

    Fundamentals model

--- models/marts/fact/fct_orders.sql
+++ models/marts/fact/fct_orders.sql
@@ -11,16 +11,33 @@ payments as (
     select * from {{ ref('stg_payments')}}
 ),
 
-fct_orders as (
+customer_order as (
 
     select  orders.order_id, 
-            customers.customer_id,
-            payments.amount
+            customers.customer_id
         from customers
-        inner join from orders using (customer_id)
-        inner join from payments using (order_id)
-        
+        inner join orders using (customer_id)
 
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select  customer_order.order_id,
+            customer_order.customer_id,
+            coalesce(order_payments.amount, 0) as amount
+        from customer_order
+        left join order_payments using (order_id)
+        
 )
 
-select * from fct_orders
+select * from final
+
